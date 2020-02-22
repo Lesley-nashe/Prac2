8f720dff4c18e6cd97e8dff70490802b59e7e555
Tadiwa Musekiwa <G17M6720@ict.ru.ac.za>
2020-02-22 21:51:51 +0200

    Prac2-commit

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -2,11 +2,25 @@
 
 namespace Problems
 {
-    class Program
+  public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string x ="";
+            Console.WriteLine(Prefix(x));
+        }
+        public static string Prefix(string input)
+        {
+            if(input.Length==0)
+            {
+                string ans = "0:0";
+                return ans;
+            }
+            int len = input.Length;
+            string[] words = input.Split(' ');
+            int numwords = words.Length;
+            string final = $"{len},{numwords}:{input}";
+            return final;
         }
     }
 }
